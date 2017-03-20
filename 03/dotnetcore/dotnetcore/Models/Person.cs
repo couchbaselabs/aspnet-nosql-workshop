88270ecf5418bf688a04ca296e9fd9cfbaec5568
mgroves <me@mgroves.com>
2017-03-20 16:37:28 -0400

    reorganizing 3 and 4 - 3 is now focused just on ASP.NET, 4 is now focused just on Angular, also started the ASP.NET Core API backend

---
+++ 03/dotnetcore/dotnetcore/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace dotnetcore.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Type => typeof(Person).Name.ToLower();
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(FirstName) &&
+                   !string.IsNullOrEmpty(LastName) &&
+                   !string.IsNullOrEmpty(Email);
+        }
+    }
+}
---
+++ 03/dotnetcore/dotnetcore/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Couchbase;
+using Couchbase.Configuration.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace dotnetcore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            // create a CORS policy that allows anyone to make a request
+            services.AddCors(o => o.AddPolicy("Anyone", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            // get config object from section in appsettings
+            var settingsSection = Configuration.GetSection("MySettings");
+            var settings = settingsSection.Get<MySettings>();
+            services.Configure<MySettings>(settingsSection);
+
+            // setup ClusterHelper for Couchbase
+            ClusterHelper.Initialize(new ClientConfiguration
+            {
+                Servers = new List<Uri> { new Uri(settings.CouchbaseServer) }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
